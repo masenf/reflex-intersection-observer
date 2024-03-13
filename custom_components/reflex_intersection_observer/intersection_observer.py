59ead373c5d34ce3af760487295f85c87036ca3e
Masen Furer <m_github@0x26.net>
2024-03-13 12:14:05 -0700

    Handle `on_non_intersect` event
    Translate some IntersectionObserverEntry props for reflex to access

--- custom_components/reflex_intersection_observer/intersection_observer.py
+++ custom_components/reflex_intersection_observer/intersection_observer.py
@@ -19,10 +19,11 @@ class IntersectionObserver(rx.el.Div):
         return {
             **super().get_event_triggers(),
             "on_intersect": lambda e0: [e0],
+            "on_non_intersect": lambda e0: [e0],
         }
 
     def _exclude_props(self) -> list[str]:
-        return ["root", "root_margin", "threshold", "on_intersect"]
+        return ["root", "root_margin", "threshold", "on_intersect", "on_non_intersect"]
 
     def _get_imports(self) -> dict[str, list[rx.utils.imports.ImportVar]]:
         return rx.utils.imports.merge_imports(
@@ -32,17 +33,36 @@ class IntersectionObserver(rx.el.Div):
             },
         )
 
+    def _get_custom_code(self) -> str | None:
+        return """
+const extractEntry = (entry) => ({
+    intersection_ratio: entry.intersectionRatio,
+    is_intersecting: entry.isIntersecting,
+    time: entry.time,
+})"""
+
     def _get_hooks(self) -> str | None:
         on_intersect = self.event_triggers.get("on_intersect")
-        if on_intersect is None:
+        on_non_intersect = self.event_triggers.get("on_non_intersect")
+        if on_intersect is None and on_non_intersect is None:
             return None
         if isinstance(on_intersect, rx.EventChain):
             on_intersect = rx.utils.format.wrap(
                 rx.utils.format.format_prop(on_intersect).strip("{}"),
                 "(",
             )
+        if isinstance(on_non_intersect, rx.EventChain):
+            on_non_intersect = rx.utils.format.wrap(
+                rx.utils.format.format_prop(on_non_intersect).strip("{}"),
+                "(",
+            )
+        if on_intersect is None:
+            on_intersect = "undefined"
+        if on_non_intersect is None:
+            on_non_intersect = "undefined"
         script_props = dict(
             on_intersect=on_intersect,
+            on_non_intersect=on_non_intersect,
             root=(
                 f"document.querySelector({rx.utils.format.format_prop(self.root).strip('{}')})"
                 if self.root is not None
@@ -58,12 +78,16 @@ class IntersectionObserver(rx.el.Div):
             ),
             ref=self.get_ref(),
         )
-        return """
+        return (
+            """
 useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                %(on_intersect)s(entry)
+            if (%(on_intersect)s && entry.isIntersecting) {
+                %(on_intersect)s(extractEntry(entry))
+            }
+            if (%(on_non_intersect)s && !entry.isIntersecting) {
+                %(on_non_intersect)s(extractEntry(entry))
             }
         });
     }, {
@@ -76,7 +100,9 @@ useEffect(() => {
         return () => observer.disconnect()
     }
 }, []);
-""" % script_props
+"""
+            % script_props
+        )
 
 
 intersection_observer = IntersectionObserver.create
