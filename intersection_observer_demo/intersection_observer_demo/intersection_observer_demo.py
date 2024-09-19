b1edf6ca5bfcc9f5bb2b79ab0bd6fa81a9edad7c
Masen Furer <m_github@0x26.net>
2024-09-18 17:11:44 -0700

    Add new scroll-to-bottom demo for chat-app use cases

--- intersection_observer_demo/intersection_observer_demo/intersection_observer_demo.py
+++ intersection_observer_demo/intersection_observer_demo/intersection_observer_demo.py
@@ -2,12 +2,18 @@ import reflex as rx
 
 from reflex_intersection_observer import intersection_observer
 
+from . import scroll_to_bottom
+
 
 BATCH_SIZE = 15
+BOTTOM_ELEMENT_ID = "bottom"
 
 
 class State(rx.State):
     items: list[str] = [f"Item {ix}" for ix in range(BATCH_SIZE)]
+
+    def reset_items(self):
+        self.reset()
 
     def handle_intersect(self, entry):
         print(f"Intersected! {entry} Load more items.")
@@ -23,12 +29,27 @@ class State(rx.State):
 
 
 def index() -> rx.Component:
-    return rx.center(
+    return rx.vstack(
+        rx.heading("Infinite Scroll Demo"),
+        rx.hstack(
+            rx.text(f"{State.items.length()} items"),
+            rx.icon_button(
+                rx.icon("rotate-ccw"),
+                on_click=State.reset_items,
+            ),
+            rx.icon_button(
+                rx.icon("arrow_down_to_line"),
+                on_click=rx.call_script(
+                    f"document.getElementById('{BOTTOM_ELEMENT_ID}').scrollIntoView()"
+                ),
+            ),
+        ),
         rx.scroll_area(
             rx.vstack(
                 rx.foreach(State.items, rx.text),
                 intersection_observer(
                     "Infinite Scroll Target",
+                    id=BOTTOM_ELEMENT_ID,
                     root="#scroller",
                     root_margin="0px",
                     threshold=0.9,
@@ -43,13 +64,20 @@ def index() -> rx.Component:
             ),
             type="hover",
             id="scroller",
-            border="1px solid black",
+            border=f"1px solid {rx.color("accent", 12)}",
             width="85vw",
             height="75vh",
         ),
-        padding_top="5em",
+        rx.text("This is outside scroll area"),
+        rx.link("Scroll to Bottom Demo", href="/scroll-to-bottom"),
+        align="center",
     )
 
 
 app = rx.App()
 app.add_page(index)
+app.add_page(
+    scroll_to_bottom.page,
+    route="/scroll-to-bottom",
+    on_load=scroll_to_bottom.MessageGenerator.on_load,
+)
